102373150a1477122239b251f53d2f200938aca7
jigar-kapadiya <jigarkapadiya6446+github@gmail.com>
2025-09-21 23:16:53 +0530

    UUID validation

--- services/api/src/users/users.resolver.ts
+++ services/api/src/users/users.resolver.ts
@@ -3,13 +3,14 @@ import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Mutation(() => User)
-  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
     return this.usersService.create(createUserInput);
   }
 
@@ -19,7 +20,7 @@ export class UsersResolver {
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(@Args('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOne(id);
   }
 
@@ -29,7 +30,7 @@ export class UsersResolver {
   }
 
   @Mutation(() => User)
-  removeUser(@Args('id', { type: () => Int }) id: number) {
+  removeUser(@Args('id', ParseUUIDPipe) id: string) {
     return this.usersService.remove(id);
   }
 }
