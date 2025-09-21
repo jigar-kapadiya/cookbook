8a55c97956892a8f5c706f49d63f3b29606ab8ab
jigar-kapadiya <jigarkapadiya6446+github@gmail.com>
2025-09-21 23:18:35 +0530

    Return only safe and public fields

--- services/api/src/users/users.service.ts
+++ services/api/src/users/users.service.ts
@@ -2,27 +2,45 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { PrismaService } from '../prisma/prisma.service';
+import { hash } from 'argon2';
+
+const userSafeSelect = {
+  id: true,
+  email: true,
+  username: true,
+  displayName: true,
+  bio: true,
+  avatarUrl: true,
+  createdAt: true,
+  updatedAt: true,
+} as const;
+
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
-  create(createUserInput: CreateUserInput) {
-    return this.prisma.users.create({ data: createUserInput });
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(createUserInput: CreateUserInput) {
+    const passwordHash = await hash(createUserInput.password, {  });
+    console.log(passwordHash)
+    return this.prisma.user.create({ data: { ...createUserInput, password: passwordHash } });
   }
 
   findAll() {
-    return this.prisma.users.findMany();
+    return this.prisma.user.findMany({
+      select: userSafeSelect,
+    });
   }
 
-  findOne(id: number) {
-    return this.prisma.users.findFirstOrThrow({ where: { id } });
+  findOne(id: string) {
+    return this.prisma.user.findFirstOrThrow({ where: { id }, select: userSafeSelect, });
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return this.prisma.users.update({ where: { id }, data: updateUserInput });
+  update(id: string, updateUserInput: UpdateUserInput) {
+    return this.prisma.user.update({ where: { id }, data: updateUserInput });
   }
 
-  remove(id: number) {
-    return this.prisma.users.delete({ where: { id } });
+  remove(id: string) {
+    return this.prisma.user.delete({ where: { id } });
   }
 }
