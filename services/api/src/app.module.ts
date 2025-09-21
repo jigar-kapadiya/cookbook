16afd2114f366e1701731429f78536c86c121976
jigar-kapadiya <jigarkapadiya6446+github@gmail.com>
2025-09-21 19:52:31 +0530

    Remove app-controller and app-service

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
@@ -26,7 +24,5 @@ import { HealthModule } from './health/health.module';
     PrismaModule,
     HealthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
